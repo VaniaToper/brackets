2157b8a1743edf07f7e53071a2f412016b241531
VaniaToper <vanjaqwe58@gmail.com>
2021-08-31 01:08:10 +0200

    feat🔨: completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const open = bracketsConfig.map((i) => i[0]);
+  const close = bracketsConfig.map((i) => i[1]);
+  let arr = [];
+  for (let i of str) {
+    const openIndex = open.indexOf(i);
+    const closeIndex = close.indexOf(i);
+    if (closeIndex === openIndex) {
+      if (arr[arr.length - 1] === open[openIndex]) {
+        arr.pop();
+      } else {
+        arr.push(open[openIndex]);
+      }
+      continue
+    }
+    if (openIndex >= 0) {
+      arr.push(openIndex);
+      continue
+    }
+    if (closeIndex != arr.pop()) {
+      return false;
+    }
+  }
+  return !arr.length
+};
